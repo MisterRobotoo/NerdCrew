6fe7a0b01aa1e8cfc51bce67f77124b6a62d1ce6
MisterRobotoo <124835924+MisterRobotoo@users.noreply.github.com>
2025-05-09 22:04:01 +0200

    huoi

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,5 @@
-function updateClock() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
-  }
+const key;
 
-  setInterval(updateClock, 1000);
-  updateClock(); // wywołanie na start
+function keyGenerate{
+	
+}
